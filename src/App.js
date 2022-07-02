7c24b547d0c33341af929a81de20cb9cc589ad41
spde3289 <spde3289@naver.com>
2022-07-03 02:17:09 +0900

    SS

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import React, { Component } from 'react';
 import './App.css';
 
 class Subject extends Component {
-  render(){
-    return (
-      <header>
-            <h1>WEB</h1>
-            world wide web!
-      </header>
+  render() {
+    return ( 
+    <header >
+      <h1> WEB </h1>
+      world wide web!
+    </header>
     );
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      Hello, React!!
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return ( 
+      <div className ="App">
+      <Subject></Subject>
+      </div>
+    );
+  };
+};
 
 export default App;
