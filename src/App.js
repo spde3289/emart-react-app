1fcd359cd3082a1ea6dfd6f3b2bf7a8745f802eb
spde3289 <spde3289@naver.com>
2022-07-05 20:54:33 +0900

    ss

--- src/App.js
+++ src/App.js
@@ -1,30 +1,62 @@
 import React, { Component } from 'react';
-import Toc from "./conponents/Toc"
-import Subject from './conponents/Subject'
-import Content from './conponents/Content'
+import Toc from "./conponents/Toc";
+import Subject from './conponents/Subject';
+import Content from './conponents/Content';
 import './App.css';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode:'read',
       subject:{title:'WEB', sub:'World Wide Web'},
+      Welcome:{title:'welcome', desc:'Hello, React!!'},
       content:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
         {id:3, title:'JS', desc:'JS is for interactive'},
       ]
-    }
-  }
+    };
+    console.log(this.state)
+    console.log(Component)
+    console.log(App)
+    
+  };
+
   render() {
+    //console.log('app render');
+    let _title, _desc = null;
+
+    if(this.state.mode === 'welcome'){
+      _title = this.state.Welcome.title;
+      _desc = this.state.Welcome.desc;
+    } else if(this.state.mode === 'read'){
+      _title =this.state.content[0].title;
+      _desc = this.state.content[0].desc;
+      console.log(this.state.mode)
+    };
+
     return ( 
       <div className ="App">
-      <Subject 
+      {/* <Subject 
         title={this.state.subject.title} 
         sub={this.state.subject.sub}>
-      </Subject>
+      </Subject> */}
+      <header>
+        <h1><a href ="/" onClick = { function(e) {
+          e.preventDefault();
+
+          console.log(this.state.mode)
+        
+          this.setState({
+            mode:'welcome'
+          })
+          //console.log(App.state)
+        }.bind(this)}>{this.state.subject.title}</a></h1>
+        {this.state.subject.sub}
+      </header>
       <Toc data={this.state.content}></Toc>
-      <Content title="HTML" desc="HTML is hyperText Markup Languang"></Content>
+      <Content title= {_title} desc= {_desc}></Content>
       </div>
     );
   };
--- src/conponents/Subject.js
+++ src/conponents/Subject.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 class Subject extends Component {
     render() {
       return ( 
-      <header >
-        <h1>{this.props.title}</h1>
+      <header>
+        <h1><a href ="/">{this.props.title}</a></h1>
         {this.props.sub}
       </header>
       );
