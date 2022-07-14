c8a3c56f01bb8fa51ff981b0d4b3562b574a23b1
spde3289 <spde3289@naver.com>
2022-07-15 08:02:21 +0900

    ss

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ class App extends Component {
     super(props);
     this.state = {
       mode:'read',
+      selected_content_id:2,
       subject:{title:'WEB', sub:'World Wide Web'},
       Welcome:{title:'welcome', desc:'Hello, React!!'},
       content:[
         {id:1, title:'HTML', desc:'HTML is for information'},
         {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JS', desc:'JS is for interactive'},
+        {id:3, title:'JS', desc:'JS is for interactive'}
       ]
     };
 
@@ -27,9 +28,16 @@ class App extends Component {
       _title = this.state.Welcome.title;
       _desc = this.state.Welcome.desc;
     } else if(this.state.mode === 'read'){
-      _title =this.state.content[0].title;
-      _desc = this.state.content[0].desc;
-
+      var i  = 0
+      while(i < this.state.content.length){
+        var data = this.state.content[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i++
+      }
     };
 
     return ( 
@@ -38,13 +46,13 @@ class App extends Component {
         title={this.state.subject.title} 
         sub={this.state.subject.sub}
         onChangePage={function(){
-          alert('hihihi');
           this.setState({mode:'welcome'})
         }.bind(this)}
         >
-        
       </Subject>}
-      <Toc data={this.state.content}></Toc>
+      <Toc onChangePage={function(id){
+        this.setState({mode:'read',selected_content_id:Number(id)})
+      }.bind(this)} data={this.state.content}></Toc>
       <Content title= {_title} desc= {_desc}></Content>
       </div>
     );
