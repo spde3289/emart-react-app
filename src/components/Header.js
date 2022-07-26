821a7c5071ad7daa14769e919b827fde594a1933
spde3289 <spde3289@naver.com>
2022-07-27 07:49:47 +0900

    ss

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+
+function Header(){
+    return (
+        <section>
+        <div>
+            <header>
+                <nav>
+                    <ul >
+                        <li>
+                            <a href="/">PEACOCK</a>
+                        </li>
+                        <li>
+                            <a href="/">이마트몰</a>
+                        </li>
+                        <li>
+                            <a href="/">신세계포인트</a>
+                        </li>
+                        <li>
+                            <a href="/">문화센터</a>
+                        </li>
+                    </ul>
+                    <ul>
+                        <li>
+                            <a href="/">My이마트</a>
+                        </li>
+                        <li>
+                            <a href="/">이마트뮤직</a>
+                        </li>
+                        <li>
+                            <a href="/">이마트TV</a>
+                        </li>
+                        <li>
+                            <a href="/">로그인</a>
+                        </li>
+                        <li>
+                            <a href="/">회원가입</a>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+        </div>
+    </section>
+    )
+}
+export default Header;
